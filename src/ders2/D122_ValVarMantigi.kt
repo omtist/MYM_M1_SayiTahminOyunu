e8b13c1f2495417d7f00b0f386c3cd5872f5de0e
Omer Taha <tahaomer010@gmail.com>
2026-07-16 17:18:52 +0200

    Ders1: Proje oluşturuldu.

---
+++ src/ders2/D122_ValVarMantigi.kt
@@ -0,0 +1,12 @@
+package ders2
+
+fun main() {
+    val uygulamaAdi = "Hava Durumu Pro"
+    // uygulamaAdi = "Hava Durumu Pro 2"
+
+    var indirimOrani = 12;
+    indirimOrani = 15;
+
+    println("Uygulama Adı: " + uygulamaAdi)
+    println("indirim oranı: " + indirimOrani)
+}
---
+++ src/ders2/D123_TipCikarimi.kt
@@ -0,0 +1,11 @@
+package ders2
+
+fun main() {
+    val sehir = "Ankara"
+    val sicaklik = 15
+    val nemOrani = 0.15
+    val dogruMu = true
+
+    var sistemMesaji : String
+
+}
